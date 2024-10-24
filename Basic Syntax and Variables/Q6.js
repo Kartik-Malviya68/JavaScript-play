520e357bce68e08f0df625dec0566dbe1aeb317a
kishan0779 <kishanmalviya1234567890@gmail.com>
2024-10-24 14:45:16 +0530

    q6.q7

---
+++ Basic Syntax and Variables/Q6.js
@@ -0,0 +1,6 @@
+function sum (a,b,c,d) {
+    return a * b 
+   
+}
+
+console.log(sum(2,3))
---
+++ Basic Syntax and Variables/Q7.js
@@ -0,0 +1,6 @@
+const name = "kishan"
+const age = 20
+const male = true
+
+
+console.log( typeof name, typeof age, typeof male)
