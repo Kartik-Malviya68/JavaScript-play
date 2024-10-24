b5d4914634e90cc4eb00eeb75266777c1e25c33b
kishan0779 <kishanmalviya1234567890@gmail.com>
2024-10-24 18:53:56 +0530

    Q16

---
+++ Arrays and Loops/Q16.js
@@ -0,0 +1,14 @@
+const kishan = [1,2,3,4,5,6]
+
+for(let i=0; i < kishan.length; i++ )
+    
+    {
+        console.log(kishan[i])
+    }
+
+ let ki = 1    
+while (ki < 10){
+    console.log("kishan")
+    ki++
+}
+
