579a55779a720675a65a6f16f6b8c9b7a824a6b0
kishan0779 <kishanmalviya1234567890@gmail.com>
2024-10-26 17:28:14 +0530

    Q26

---
+++ Functions and Objects/Q26.js
@@ -0,0 +1,13 @@
+function user(name, age){
+        return { 
+            name: name,
+            age: age
+}}
+
+function  objkey(obj) {
+return object(obj)
+} 
+
+const person = user("kishan", 21)
+
+console.log(person)
