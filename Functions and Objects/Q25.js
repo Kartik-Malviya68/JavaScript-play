5c0c03cc0c6a1292fed234e71a5017ebff076cc9
kishan0779 <kishanmalviya1234567890@gmail.com>
2024-10-26 17:07:09 +0530

    Q25

---
+++ Functions and Objects/Q25.js
@@ -0,0 +1,12 @@
+const user = {
+    name: "kishan",
+    age: 21,
+    gender: "male",
+    pincode: 395010,
+}
+
+
+console.log(user.name)
+console.log(user.age)
+console.log(user["gender"])
+console.log(user["pincode"])
