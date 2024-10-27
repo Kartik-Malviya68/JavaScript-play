93515db35d82a65a61f77573aa2d541635c74b8f
kishan0779 <kishanmalviya1234567890@gmail.com>
2024-10-27 17:30:16 +0530

    q33

---
+++ Conditionals and Logic/Q33.js
@@ -0,0 +1,19 @@
+const fruit = "mango";
+
+switch (fruit) {
+    case 'apple' :
+        console.log("red apple");
+        break;
+    case "banana" : 
+        console.log("banana is yellow");  
+        break;
+    case "mango" : 
+        console.log("mango is yellow");  
+        break;
+    case "orange" : 
+        console.log("orange is orange");  
+        break;
+    default : 
+        console.log("default value");  
+        break;
+}
