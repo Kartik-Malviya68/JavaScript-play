c3bf06e379543672fac05a7825cf7907fbdcb745
kishan0779 <kishanmalviya1234567890@gmail.com>
2024-10-28 16:30:09 +0530

    Q36

---
+++ Conditionals and Logic/Q36.js
@@ -0,0 +1,17 @@
+const number = 42;
+const string = "hii"; 
+const boolean = true;
+const object = { key: "value" };
+const array = [1, 2, 3];
+const func = function() { return "function"; }; 
+const undef = undefined;
+const nul = null;
+
+console.log(typeof number); 
+console.log(typeof string);  
+console.log(typeof boolean);  
+console.log(typeof object);  
+console.log(typeof array);     
+console.log(typeof func);     
+console.log(typeof undef);    
+console.log(typeof nul);     
